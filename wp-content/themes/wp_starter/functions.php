a359e4da2d2b7dfe8a126257188dd3b4d0b8389f
Radomir Bednar <radovanmail@gmail.com>
2015-10-19 12:02:37 +0200

    remove emoji by default

--- wp-content/themes/wp_starter/functions.php
+++ wp-content/themes/wp_starter/functions.php
@@ -151,3 +151,12 @@ require get_template_directory() . '/inc/jetpack.php';
 
 
 add_theme_support('post-thumbnails');
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+
+
+
