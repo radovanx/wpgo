8e2301db946d5f40efa3e7dd5d845c13818f2009
Radomir Bednar <radovanmail@gmail.com>
2015-10-19 12:06:32 +0200

    Update functions.php

--- wp-content/themes/wp_starter/functions.php
+++ wp-content/themes/wp_starter/functions.php
@@ -148,9 +148,32 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
-
-
+ 
 add_theme_support('post-thumbnails');
+
+
+  // =========================================================================
+    // REMOVE JUNK FROM HEAD
+    // =========================================================================
+    remove_action('wp_head', 'rsd_link'); // remove really simple discovery link
+    remove_action('wp_head', 'wp_generator'); // remove wordpress version
+
+    remove_action('wp_head', 'feed_links', 2); // remove rss feed links (make sure you add them in yourself if youre using feedblitz or an rss service)
+    remove_action('wp_head', 'feed_links_extra', 3); // removes all extra rss feed links
+
+    remove_action('wp_head', 'index_rel_link'); // remove link to index page
+    remove_action('wp_head', 'wlwmanifest_link'); // remove wlwmanifest.xml (needed to support windows live writer)
+
+    remove_action('wp_head', 'start_post_rel_link', 10, 0); // remove random post link
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0); // remove parent post link
+    remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // remove the next and previous post links
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
+
+/*
+    remove smiles and smile
+*/
 
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
