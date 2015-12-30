d78b1dcb070ca5248bb0321ed00af795d1299ff0
Radomir Bednar <radovanmail@gmail.com>
2015-12-30 19:01:29 +0100

    some formating

--- wp-content/themes/wp_starter/functions.php
+++ wp-content/themes/wp_starter/functions.php
@@ -152,9 +152,9 @@ require get_template_directory() . '/inc/jetpack.php';
 add_theme_support('post-thumbnails');
 
 
-  // =========================================================================
-    // REMOVE JUNK FROM HEAD
-    // =========================================================================
+/* 
+ * Remove junk from head
+*/
     remove_action('wp_head', 'rsd_link'); // remove really simple discovery link
     remove_action('wp_head', 'wp_generator'); // remove wordpress version
 
@@ -171,10 +171,9 @@ add_theme_support('post-thumbnails');
 
     remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
-/*
-    remove smiles and smile
+/** 
+* remove smiles and smile
 */
-
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
